0b7da9541496c053c364526f7b4d1dd51be75d29
Sandy Green <green7786@comcast.net>
2018-06-21 11:06:12 -0600

    added jquery for mobile hover effect on touch

---
+++ jquery.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+  $(".paw-logo" ).on("mouseenter mouseleave touchstart", function(e){
+    if(e.type == 'touchstart') {
+      $(this).off('mouseenter mouseleave');
+    }
+
+    $(this).toggleClass("hover");
+  });
+
+
+
+  }); //end ready
