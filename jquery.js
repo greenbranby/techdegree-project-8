4682de93576966bf20603b3477d7be4c9bc1fb3d
Sandy Green <green7786@comcast.net>
2018-06-21 11:01:12 -0600

    more testing

--- jquery.js
+++ jquery.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
 
-  $(".paw-logo").on("mouseenter mouseleave touchstart", function(e){
+  $(".paw-logo", ".sable", ".fawn", ".red", '.stone').on("mouseenter mouseleave touchstart", function(e){
     if(e.type == 'touchstart') {
       $(this).off('mouseenter mouseleave');
     }
 
     $(this).toggleClass("hover");
   });
+
+
 
   }); //end ready
