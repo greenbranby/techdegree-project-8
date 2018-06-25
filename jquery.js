9554117570efb92973c05612a5beeda59b90bd09
Sandy Green <green7786@comcast.net>
2018-06-24 18:59:28 -0600

    fixed nav hover errors

--- jquery.js
+++ jquery.js
@@ -8,34 +8,6 @@ $(document).ready(function () {
     $(this).toggleClass("hover");
   });
 
-  $(".sable" ).on("mouseenter mouseleave touchstart", function(e){
-    if(e.type == 'touchstart') {
-      $(this).off('mouseenter mouseleave');
-    }
-
-    $(this).toggleClass("hover");
-  });
-  $(".fawn" ).on("mouseenter mouseleave touchstart", function(e){
-    if(e.type == 'touchstart') {
-      $(this).off('mouseenter mouseleave');
-    }
-
-    $(this).toggleClass("hover");
-  });
-  $(".red" ).on("mouseenter mouseleave touchstart", function(e){
-    if(e.type == 'touchstart') {
-      $(this).off('mouseenter mouseleave');
-    }
-
-    $(this).toggleClass("hover");
-  });
-  $(".stone" ).on("mouseenter mouseleave touchstart", function(e){
-    if(e.type == 'touchstart') {
-      $(this).off('mouseenter mouseleave');
-    }
-
-    $(this).toggleClass("hover");
-  });
 
 
   }); //end ready
